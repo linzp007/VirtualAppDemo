031744993a7b5d7d6e1bed3fc413db1247a39ef9
keyongyu <keyongyu@ifunbow.com>
2017-09-18 10:03:51 +0800

    相机

--- plugin1/src/main/java/com/kk/plugin1/MainActivity.java
+++ plugin1/src/main/java/com/kk/plugin1/MainActivity.java
@@ -1,7 +1,11 @@
 package com.kk.plugin1;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -9,6 +13,9 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.webkit.WebView;
+
+import java.io.File;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-        ((WebView)findViewById(R.id.webview)).loadUrl("http://ip.cn");
+        ((WebView) findViewById(R.id.webview)).loadUrl("http://ip.cn");
         findViewById(R.id.test_service).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -37,6 +44,17 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, NotificationActivity.class));
+            }
+        });
+        findViewById(R.id.test_camera).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                String photofileName = "IMG_" + new Date() + ".jpg";
+                File imgFile = new File(Environment.getExternalStorageDirectory(),
+                        "tempWhy/" + photofileName);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(imgFile));
+                startActivityForResult(intent, 1111);
             }
         });
     }
@@ -61,5 +79,39 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            switch (requestCode) {
+                //拍照功能
+                case 1111:
+                    startPhotoZoom(data.getData());
+                    break;
+            }
+        }
+    }
+
+    private void startPhotoZoom(Uri uri) {
+
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        intent.setDataAndType(uri, "image/*");
+
+        //设置是否裁剪
+        intent.putExtra("crop", "true");
+
+        //设置宽高的比例
+        intent.putExtra("aspectX", 1);
+        intent.putExtra("aspectY", 1);
+
+        //设置裁剪图片的宽高：影响效果
+        intent.putExtra("outputX", 200);
+        intent.putExtra("outputY", 200);
+        intent.putExtra("return-data", true);
+        intent.putExtra("output", uri);
+        intent.putExtra("outputFormat", "JPEG");
+        startActivityForResult(intent, 1112);
     }
 }
